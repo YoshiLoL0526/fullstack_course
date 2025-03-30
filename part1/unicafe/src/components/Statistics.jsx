e0332032de374953692fcfe6837759416eaae421
Raidel <yoshilol0526@gmail.com>
2025-03-29 22:29:21 -0300

    Only show statistics when feedback given

--- part1/unicafe/src/components/Statistics.jsx
+++ part1/unicafe/src/components/Statistics.jsx
@@ -3,6 +3,15 @@ const Statistics = ({ feedback }) => {
     const average = feedback.good * 1 + feedback.bad * -1 + feedback.neutral * 0
     const positive = (feedback.good / all) * 100
 
+    if (all === 0) {
+        return (
+            <div>
+                <h1>Statistics</h1>
+                <p>No feedback given</p>
+            </div>
+        )
+    }
+
     return (
         <div>
             <h1>Statistics</h1>
